43ee8034c0b9b1f6f227f4c6f705c7f8e187d7ad
Сергей Батурин <94468513+BaturinSS@users.noreply.github.com>
2022-08-31 02:35:00 +0300

    ++

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className="footer">
       <p className="footer__copyright">
-        © 2022 - {new Date().getFullYear()}. Батурин Сергей
+        © {new Date().getFullYear()}. Батурин Сергей
       </p>
     </div>
   )
